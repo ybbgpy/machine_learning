25ae6fc1f020785fd7b9b77333656fe706184c21
ybbgpy <yuanbinjohnson@gmail.com>
2013-11-04 15:45:58 +0800

    Update linear_regression(lms).m
    change the converge condition

--- linear_regression(lms).m
+++ linear_regression(lms).m
@@ -8,7 +8,9 @@ noise=normrnd(noise_mean,noise_var,1,N);
 
 y_measure=y_real+noise;
 
+
 plot(x,y_real,'g');
+axis([0 1 0 5]);
 hold on
 plot(x,y_measure,'o');
 
@@ -18,29 +20,24 @@ theta=[0,0];
 %learning rate
 alpha=0.001;
 
-%mean_square_threshold
-mean_square_thres=0.03;
+%theta_threshold
+theta_thres=0.01;
 
 count=0;
 
 while 1
     
     %calculate the mean square
-    sum_square=0;
-    for i=1:N
-        h=theta(1)+theta(2)*x(i);
-        sum_square=sum_square+(y_measure(i)-h)^2;
-    end
-    mean_square=sum_square/N;
-    disp(mean_square);
-    if mean_square<mean_square_thres
-        break;
-    end
-    
-    count=count+1;
-    if count>1000 
-        alpha=alpha/10;
-    end
+%     sum_square=0;
+%     for i=1:N
+%         h=theta(1)+theta(2)*x(i);
+%         sum_square=sum_square+(y_measure(i)-h)^2;
+%     end
+%     mean_square=sum_square/N;
+%     %disp(mean_square);
+%     if mean_square<mean_square_thres
+%         break;
+%     end
     
     delta=[0,0];
     for i=1:N
@@ -48,13 +45,16 @@ while 1
         delta(1)=delta(1)+(y_measure(i)-h);
         delta(2)=delta(2)+(y_measure(i)-h)*x(i);
     end
+    if delta(1)<theta_thres && delta(2)<theta_thres
+        break;
+    end
     %disp(delta);
     theta=theta+alpha*delta;
     %disp(count);
     %disp(theta);
 end
 
-disp(theta)
+disp(theta);
 y_hypo=theta(1)+theta(2)*x;
 plot(x,y_hypo,'r');
 hold off
